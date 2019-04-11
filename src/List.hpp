1dd22309766bd5a22629f3b0630505d6e810129b
Laykel <layke.luc@gmail.com>
2019-04-11 16:22:36 +0200

    Headers

--- src/List.hpp
+++ src/List.hpp
@@ -1,6 +1,14 @@
-//
-// Created by luc on 11.04.19.
-//
+/* ---------------------------------------------------------------------------------
+ * Lab         : Laboratoire 3 - Liste
+ * File        : List.cpp
+ * Author(s)   : Alison Savary et Luc Wachter
+ * Date        : 11.04.2019
+ *
+ * Goal        :
+ *
+ * Compilateur : g++ version 8.2.0
+ * ---------------------------------------------------------------------------------
+ */
 
 #ifndef LIST_HPP
 #define LIST_HPP
